296cfc4feeb5c837504a80d56ea66c0c1b6bef47
Jorge <me.mark.two@gmail.com>
2015-12-09 19:26:44 -0500

    feat: get file list and load file contents done with jquery

--- RemoteTextEditor.js
+++ RemoteTextEditor.js
@@ -87,32 +87,25 @@ function RemoteTextEditor(textBoxID, textFileSelectID, nameTextBoxID) {
 	Return: Void
 	*/
 	this.GetFileList = (function() {
-		// Create response function
-		this.xmlhttp.onreadystatechange = (function(){
-			if (this.xmlhttp.readyState == 4 && this.xmlhttp.status == 200) {
-				var response = this.xmlhttp.responseText;
-				var filesArray = JSON.parse(response);
-				var selectElement = document.getElementById(this.textFileSelectID);
-				var selectOptions = "";
+		// Create ajax request
+		$.ajax({ url: "GetFileList.php" }).done((function(response){
+			var filesArray = JSON.parse(response); // The new options
+			var selectElement = $("#" + this.textFileSelectID)[0];
+			var selectOptions = "";
 
-				while (selectElement.length > 0) {
-					selectElement.remove(0);
-				}
-
-				for (var file = 0; file < filesArray.length; file++) {
-					var option = document.createElement("OPTION");
-					option.value = filesArray[file];
-					option.text = filesArray[file];
-					selectElement.add(option);
-				};
+			// Clear current list
+			while (selectElement.length > 0) {
+				selectElement.remove(0);
 			}
-		}).bind(this)
 
-		// Create request
-		this.xmlhttp.open("GET", "GetFileList.php");
-
-		// send request
-		this.xmlhttp.send(null);
+			// Add new options to list
+			for (var file = 0; file < filesArray.length; file++) {
+				var option = document.createElement("OPTION");
+				option.value = filesArray[file];
+				option.text = filesArray[file];
+				selectElement.add(option);
+			};
+		}).bind(this))
 	}).bind(this)
 
 
@@ -127,24 +120,16 @@ function RemoteTextEditor(textBoxID, textFileSelectID, nameTextBoxID) {
 	Return: Void
 	*/
 	this.LoadFileContents = (function() {
-		// Create response function
-		this.xmlhttp.onreadystatechange = (function(){
-			if (this.xmlhttp.readyState == 4 && this.xmlhttp.status == 200) {
-				document.getElementById(this.textBoxID).value = this.xmlhttp.responseText;
-			}
-		}).bind(this)
-
 		// File to request
-		var filePath = this.filesDir + document.getElementById(this.textFileSelectID).value
+		var filePath = this.filesDir + $("#" + this.textFileSelectID)[0].value
 
 		// Add a unique identifier to bypass caching of the file
 		filePath += '?_=' + new Date().getTime()
 
 		// Create request
-		this.xmlhttp.open("GET", filePath);
-
-		// send request
-		this.xmlhttp.send(null);
+		$.ajax({ url: filePath }).done((function(response){
+			$("#" + this.textBoxID)[0].value = response;
+		}).bind(this))
 	}).bind(this)
 
 
@@ -164,17 +149,17 @@ function RemoteTextEditor(textBoxID, textFileSelectID, nameTextBoxID) {
         var namePatt = /^[0-9a-zA-Z ]+$/;
 
         // Get the file name
-        var fileName = document.getElementById(this.nameTextBoxID).value;
+        var fileName = $("#" + this.nameTextBoxID).value;
 
         // Compare the input to the pattern.
         fileName = fileName.match(namePatt);
 
         if (fileName == null) {
         	// Display invalid file name message
-        	document.getElementById(this.errorBoxID).innerHTML = "That file name is not valid. Only letters, number and spaces are allowed.";
+        	$("#" + this.errorBoxID).innerHTML = "That file name is not valid. Only letters, number and spaces are allowed.";
         } else {
         	// Clear invalid file name message
-        	document.getElementById(this.errorBoxID).innerHTML = ""
+        	$("#" + this.errorBoxID).innerHTML = ""
         	valid = true
         }
 
